070601d006408dbb82477473d27044af7419315e
Rena Sakata <rena.sakata.cn@gmail.com>
2023-05-27 21:57:18 -0700

    display auth status

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
 
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 import AppleHealthKit from 'react-native-health';
+import { NativeEventEmitter, NativeModules } from 'react-native';
 
 /* Permission options */
 const permissions = {
   permissions: {
-    read: ['HKQuantityTypeIdentifierStepCount'],
-    // write: ['HKQuantityTypeIdentifierStepCount'],
+    read: [AppleHealthKit.Constants.Permissions.Steps],
+    write: [AppleHealthKit.Constants.Permissions.Steps],
   },
 };
 
@@ -20,45 +29,102 @@ AppleHealthKit.initHealthKit(permissions, (error) => {
 
   /* Can now read or write to HealthKit */
 
-  const options = {
-    startDate: new Date(2020, 1, 1).toISOString(),
+  let options = {
+    startDate: new Date(2016, 1, 1).toISOString(), // required
+    endDate: new Date().toISOString(), // optional; default now
   };
 
-  AppleHealthKit.getHeartRateSamples(options, (callbackError, results) => {
-    /* Samples are now collected from HealthKit */
+  AppleHealthKit.getDailyStepCountSamples(options, (err, results) => {
+    if (err) {
+      return;
+    }
+    console.log('getDailyStepCountSamples result: ', results);
   });
 });
 
-let options = {
-  date: new Date().toISOString(), // optional; default now
-  includeManuallyAdded: false, // optional: default true
-};
-
-let steps;
-
-AppleHealthKit.getStepCount(options, (err, results) => {
-  if (err) {
-    console.log(err.message);
-    return;
-  }
-  console.log(results);
-  steps = results.value;
-});
-
 export default function App() {
+  const [authStatus, setAuthStatus] = useState({});
+
+  useEffect(() => {
+    new NativeEventEmitter(NativeModules.AppleHealthKit).addListener(
+      'healthKit:StepCount:new',
+      async () => {
+        console.log('--> observer triggered');
+      }
+    );
+  }, []);
+
+  const handlePressGetAuthStatus = () => {
+    AppleHealthKit.getAuthStatus(permissions, (err, result) => {
+      if (err) {
+        console.error(err);
+      }
+      setAuthStatus(result);
+    });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Steps: {steps}</Text>
-      <StatusBar style='auto' />
-    </View>
+    <>
+      <StatusBar barStyle='dark-content' />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior='automatic'
+          style={styles.scrollView}
+        >
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>
+                React Native Health Example
+              </Text>
+              <Text onPress={handlePressGetAuthStatus}>
+                Press me to get Auth Status
+              </Text>
+              <Text style={styles.sectionDescription}>
+                {JSON.stringify(authStatus, null, 2)}
+              </Text>
+            </View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
   },
 });
